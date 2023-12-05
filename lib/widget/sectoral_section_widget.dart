c7cf516ffa9bbf2dc0c325078b02919f7a18ce9b
Henry Augusta <henryaugusta4@gmail.com>
2023-12-05 10:28:45 +0700

    add google fonts, fix ui.

--- lib/widget/sectoral_section_widget.dart
+++ lib/widget/sectoral_section_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:vinansia_lite/data/snip_response_dto.dart';
 import 'package:vinansia_lite/data/unboxing_sectoral_response_dto.dart';
 import 'package:vinansia_lite/widget/unboxing_item_widget.dart';
@@ -17,24 +18,46 @@ class SectoralSectionWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     List<Widget> listWidget = [];
+
+    var filterIds = [206, 205, 34, 203, 4, 202, 199, 204]; // IDs to filter b
     datas.forEach((element) {
       var widget = Container(
-        height: 210,
+        height: 230,
         width: 200,
         child: Card(
-          color: Colors.white,
+          color: Color(0xFFFFFFFF),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              Text(element.title ?? ""),
-              Text(element.dateDisplay ?? ""),
-              SizedBox(height: 20,),
+              SizedBox(
+                height: 10,
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 10, right: 10),
+                child: Text(
+                  element.title ?? "",
+                  style: GoogleFonts.montserrat().copyWith(
+                      color: Colors.black,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w900),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 10, right: 10),
+                child: Text(element.dateDisplay ?? ""),
+              ),
+              SizedBox(
+                height: 20,
+              ),
               Expanded(
                 child: ClipRRect(
-                  borderRadius: BorderRadius.only(bottomLeft: Radius.circular(8),bottomRight: Radius.circular(8)), // Adjust the border radius as needed
+                  borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(8),
+                      bottomRight: Radius.circular(8)),
+                  // Adjust the border radius as needed
                   child: Image.network(
                     element.thumbnailUrl ?? "",
-                    fit: BoxFit.fill, // Maintain aspect ratio and cover the available space
+                    fit: BoxFit.fill,
                   ),
                 ),
               ),
@@ -43,11 +66,12 @@ class SectoralSectionWidget extends StatelessWidget {
         ),
       );
 
-      listWidget.add(widget);
+      //filter certain item to be shown.
+      if (filterIds.contains(element.id ?? 0)) listWidget.add(widget);
     });
 
     return Container(
-      height: 210,
+      height: 230,
       child: ListView(
         // This next line does the trick.
         scrollDirection: Axis.horizontal,
